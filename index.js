60fb8d1f4b454d3f2acd7cb59882401ca64c7458
NattawatTHwork <nattawatth.work@gmail.com>
2023-07-10 16:46:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection(process.env.DATABASE_URL)
 
 app.get('/', (req, res) => {
     console.log('Hello world')
-    res.send('Hello world!!')
+    res.send('รักนะคะ จุ้บๆ')
 })
 
 app.get('/attractions', (req, res) => {
